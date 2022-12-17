5e1a7356c7c9bf50441d35dce496d730c8cea227
Dahee Jeong <jsssss683@gmail.com>
2022-12-18 02:09:41 +0900

    edit

--- src/components/ButtlerAnt.tsx
+++ src/components/ButtlerAnt.tsx
@@ -1,43 +1,49 @@
 import React, {
-    useState,
-    useCallback,
-    useRef,
-    useEffect,
-    useMemo,
-} from 'react';
-import _ from 'lodash';
-import Modal from 'react-modal';
-import { fetcher, postFetcher, putFetcher } from '../helper/Helper';
-import { useSession } from 'next-auth/react';
+  useState,
+  useCallback,
+  useRef,
+  useEffect,
+  useMemo,
+} from "react";
+import _ from "lodash";
+import Modal from "react-modal";
+import { fetcher, postFetcher, putFetcher } from "../helper/Helper";
+import { useSession } from "next-auth/react";
 import InfoModal from "components/modals/InfoModal";
 
 type Props = {
-    letters: LetterType[];
-    userList?: string[];
+  letters: LetterType[];
+  userList?: string[];
 };
 
 export default function GiftBox(props: Props) {
-    const { letters, userList } = props;
-    const { data: session } = useSession();
-    const user = session?.user;
-    const userName =  user?.name;
+  const { letters, userList } = props;
+  const { data: session } = useSession();
+  const user = session?.user;
+  const userName = user?.name;
 
-    const [showInfo, setShowInfo] = useState(false);
+  const [showInfo, setShowInfo] = useState(false);
 
-    return (
-        <>
-            <InfoModal userList={userList} letters={letters} open={showInfo} close={() => setShowInfo(false)} />
-            <div className="absolute right-40 bottom-[200px] w-1/5 h-1/5">
-                <button onClick={() => setShowInfo(true)}>
-                    <img src="/images/gami.png" className="w-full h-full" />
-                </button>
-                <div className="absolute py-4 px-9 top-[-50%] border-4 border-black rounded-full bg-white z-10">
-                <h6 className="text-black text-center leading-5">
-                    안녕하세요! {userName} 주인님! 당신의 집사 일개미예용.. 벽난로 앞에
-                    선물이 도착했어요! 함 열어보세용!ㅋㅋ
-                </h6>
-                </div>
-            </div>
-        </>
-    );
+  return (
+    <>
+      <InfoModal
+        userList={userList}
+        letters={letters}
+        open={showInfo}
+        close={() => setShowInfo(false)}
+      />
+      <div className="absolute right-40 bottom-[200px] w-1/5 h-1/5">
+        <button onClick={() => setShowInfo(true)}>
+          <img src="/images/gami.png" className="w-full h-full" />
+        </button>
+        <div className="absolute py-4 px-9 top-[-50%] border-2 border-black rounded w-fit bg-white z-10">
+          <h6 className="text-black text-center leading-5 w-fit whitespace-nowrap">
+            안녕하세요! {userName}님의 집사 일개미예요.
+            <br />
+            벽난로 앞에 선물이 도착했어요! 얼른 확인해보세요.
+          </h6>
+        </div>
+      </div>
+    </>
+  );
 }
