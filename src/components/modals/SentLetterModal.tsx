74d13f59342c3ab98a6afb01ec4f928efd425ef4
Dahee Jeong <jsssss683@gmail.com>
2022-12-18 18:47:45 +0900

    승국

--- src/components/modals/SentLetterModal.tsx
+++ src/components/modals/SentLetterModal.tsx
@@ -45,7 +45,8 @@ export default function SentLetterModal(props: Props) {
         <div className="p-4 flex flex-col justify-center">
           <h4 className="leading-8">
             {reciever}님에게
-            <br />[ {present && presentName} ]<br />와 함께 편지를 보냈어요.
+            <br />[ {present && presentName} ]<br />
+            과(와) 함께 편지를 보냈어요.
           </h4>
         </div>
         <button
